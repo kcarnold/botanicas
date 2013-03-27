e9172489c3cdeefaa60a9f01dca2560541e44bc5
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-03-27 18:48:19 -0400

    Server-side add code.

--- api/trees/index.php
+++ api/trees/index.php
@@ -1,10 +1,29 @@
 <?php
 require '../../db.php';
 
-$trees = array();
-foreach ($dbh->query("SELECT * FROM trees") as $row) {
-    $trees[] = array('id' => $row['id'], 'latlong' => $row['latlong'], 'name' => $row['name']);
+function getPostedModel() {
+    return json_decode($_POST['model'], true); // decode to associative arrays.
 }
-header('Content-Type: application/json; charset=utf8');
-echo json_encode($trees);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Only creating new trees is supported for now.
+    $newTree = getPostedModel();
+    $stmt = $dbh->prepare("INSERT INTO trees (name, latlong) VALUES (?, ?)");
+    $success = $stmt->execute(array($newTree['name'], $newTree['latlong']));
+    if ($success) {
+        echo 'ok';
+    } else {
+        echo "Error adding the tree\n\n";
+        var_dump($newTree);
+        http_response_code('500');
+    }
+} else {
+    $trees = array();
+    foreach ($dbh->query("SELECT * FROM trees") as $row) {
+        $trees[] = array('id' => $row['id'], 'latlong' => $row['latlong'], 'name' => $row['name']);
+    }
+    header('Content-Type: application/json; charset=utf8');
+    echo json_encode($trees);
+}
+
 ?>
